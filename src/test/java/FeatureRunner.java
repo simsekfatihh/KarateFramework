978a1cf4da8e76002e8aa60f504e82e7ea868b10
fatih <simsekfatih520@gmail.com>
2023-01-25 13:25:54 +0100

    try

---
+++ src/test/java/FeatureRunner.java
@@ -0,0 +1,14 @@
+import com.intuit.karate.junit4.Karate;
+import com.intuit.karate.KarateOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Karate.class)
+@KarateOptions(
+        tags="@wip"
+)
+public class FeatureRunner {
+
+
+
+
+}
---
+++ src/test/java/Features/FirstKarateTest.feature
@@ -0,0 +1,7 @@
+Feature: Hello Karate
+  
+  @wip
+  Scenario: How to print
+    Given print "Hello Karate"
+    When  print "Hello Student"
+    Then print  " i hope i will get the job soon"
